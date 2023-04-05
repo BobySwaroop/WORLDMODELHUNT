b9132d3d6a105992e449424dcb1e1f62a235900d
BobySwaroop <bobyswaroop555024@gmail.com>
2023-04-05 22:45:10 +0530

    delete add

--- src/Components/Navbar/Card.jsx
+++ src/Components/Navbar/Card.jsx
@@ -1,19 +1,45 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useFirebase } from "../firebase-config";
+import { useFirebase ,db } from "../firebase-config";
+import { doc, deleteDoc, collection, getDocs } from "firebase/firestore"
 import "../../App.css";
 
 const Card = (props) => {
+  const [array, setarray] = useState([]);
+  const databse = collection(db, "imageUploads")
   const firebase = useFirebase();
 
   const [url, setURL] = useState(null);
   useEffect(() => {
     firebase.getImageURL(props.url).then((url) => setURL(url));
+    getData();
+  
   }, []);
-
+  
   const handleDownloadClick = (url) => {
     window.open(url, "_blank");
   };
+  // fetch the data
+  const getData = async () =>{
+    const data = await getDocs(databse);
+    setarray(data.docs.map((item)=> {
+      return { ...item.data(), id: item.id }
+    }))
+  }
+  // delete the record
+  const handleDeleteClick = (id) => {
+    let deletedata = doc(db, "imageUploads", id)
+    deleteDoc(deletedata)
+    .then(()=> {
+      alert("Delete Record");
+    })
+    .catch((err)=> {
+      console.log(err);
+    })
+  }
+  {array.map((item) => {
+
+  
   return (
     
     <div className="container bg-light w-75 main-contianer loadata mt-4">
@@ -45,7 +71,7 @@ const Card = (props) => {
               </span>
               {props.caption}
             </p>
-            <Link path="#" className="btn btn-danger m-2 float-end">
+            <Link path="#" className="btn btn-danger m-2 float-end" onClick={ () => handleDeleteClick(item.id)}>
               Delete
             </Link>
             <Link path="#" className="btn btn-success m-2 float-end" onClick={() => handleDownloadClick(props.url)}>
@@ -56,6 +82,7 @@ const Card = (props) => {
       </div>
     </div>
   );
+})}
 };
 
 export default Card;
